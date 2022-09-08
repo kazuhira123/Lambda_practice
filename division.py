85dd7696182b19cd0e2a73320d40b7e5dbf06a1c
Izumi1997 <kohey-bsk.0720@outlook.com>
2022-09-08 23:27:14 +0900

    解説コメント追加

--- division.py
+++ division.py
@@ -1,8 +1,8 @@
 import json #jsonモジュールをimport
 def myfunc01_handler(event, context):
-  x = int(event["x"])
+  x = int(event["x"]) #event引数で渡された値を整数型に変換
   y = int(event["y"])
-  print("x = " + str(x))
+  print("x = " + str(x)) #引数に渡された値を文字列として標準出力する
   print("y = " + str(y))
   retval = {"result" : x / y}
   return json.dumps(retval) #JSON形式で結果を返す
